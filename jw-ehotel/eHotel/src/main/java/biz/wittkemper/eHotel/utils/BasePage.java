1f8529e3e8cef8dc17d9c23e2bee4f9069c359cb
joerg@wittkemper.biz <joerg@wittkemper.biz@7fa40d2c-feb0-e616-d173-2ff4c8531f6c>
2013-12-29 13:18:27 +0000

    kleinere Anpassungen am Layout

--- jw-ehotel/eHotel/src/main/java/biz/wittkemper/eHotel/utils/BasePage.java
+++ jw-ehotel/eHotel/src/main/java/biz/wittkemper/eHotel/utils/BasePage.java
@@ -1,9 +1,18 @@
 package biz.wittkemper.eHotel.utils;
 
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class BasePage extends WebPage {
 
 	private static final long serialVersionUID = -7120613976566404345L;
 
+	public BasePage() {
+		this(new PageParameters());
+	}
+
+	public BasePage(PageParameters parameters) {
+		add(new Label("PageTitle", "eHotel"));
+	}
 }
