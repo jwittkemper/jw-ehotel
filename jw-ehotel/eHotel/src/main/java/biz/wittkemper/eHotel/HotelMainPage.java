ef258e6065dda2af6d68f24abdac62a4c528d93e
joerg@wittkemper.biz <joerg@wittkemper.biz@7fa40d2c-feb0-e616-d173-2ff4c8531f6c>
2013-12-29 12:56:04 +0000

    Style eingefügt

--- jw-ehotel/eHotel/src/main/java/biz/wittkemper/eHotel/HotelMainPage.java
+++ jw-ehotel/eHotel/src/main/java/biz/wittkemper/eHotel/HotelMainPage.java
@@ -1,13 +1,13 @@
 package biz.wittkemper.eHotel;
 
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
-import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 
+import biz.wittkemper.eHotel.utils.BasePage;
 import biz.wittkemper.eHotel.utils.SignOut;
 
 @AuthorizeInstantiation("ADMIN")
-public class HotelMainPage extends WebPage {
+public class HotelMainPage extends BasePage {
 
 	private static final long serialVersionUID = -4634526324103057329L;
 
---
+++ jw-ehotel/eHotel/src/main/java/biz/wittkemper/eHotel/utils/BasePage.java
@@ -0,0 +1,9 @@
+package biz.wittkemper.eHotel.utils;
+
+import org.apache.wicket.markup.html.WebPage;
+
+public class BasePage extends WebPage {
+
+	private static final long serialVersionUID = -7120613976566404345L;
+
+}
