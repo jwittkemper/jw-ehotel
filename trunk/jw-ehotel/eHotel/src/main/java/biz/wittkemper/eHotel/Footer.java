6c8b1571748372175435a318720282f80b6d66bc
joerg@wittkemper.biz <joerg@wittkemper.biz@7fa40d2c-feb0-e616-d173-2ff4c8531f6c>
2013-12-30 13:02:09 +0000

    Diverse Komponenten erzeugt und eingebunden.

---
+++ trunk/jw-ehotel/eHotel/src/main/java/biz/wittkemper/eHotel/Footer.java
@@ -0,0 +1,13 @@
+package biz.wittkemper.eHotel;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+public class Footer extends Panel {
+
+	private static final long serialVersionUID = 4428288145137491662L;
+
+	public Footer(String id) {
+		super(id);
+	}
+
+}
--- trunk/jw-ehotel/eHotel/src/main/java/biz/wittkemper/eHotel/utils/BasePage.java
+++ trunk/jw-ehotel/eHotel/src/main/java/biz/wittkemper/eHotel/utils/BasePage.java
@@ -4,6 +4,9 @@ import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
+import biz.wittkemper.eHotel.Footer;
+import biz.wittkemper.eHotel.Header;
+
 public class BasePage extends WebPage {
 
 	private static final long serialVersionUID = -7120613976566404345L;
@@ -13,6 +16,8 @@ public class BasePage extends WebPage {
 	}
 
 	public BasePage(PageParameters parameters) {
+		add(new Header("header"));
 		add(new Label("PageTitle", "eHotel"));
+		add(new Footer("footer"));
 	}
 }
