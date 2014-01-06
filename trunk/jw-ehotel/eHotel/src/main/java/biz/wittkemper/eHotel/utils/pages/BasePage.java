2af2fa669220881af1f62ed03af2cab891698f82
joerg@wittkemper.biz <joerg@wittkemper.biz@7fa40d2c-feb0-e616-d173-2ff4c8531f6c>
2014-01-06 21:21:21 +0000

    Bootstrap eingebunden....

--- trunk/jw-ehotel/eHotel/src/main/java/biz/wittkemper/eHotel/utils/pages/BasePage.java
+++ trunk/jw-ehotel/eHotel/src/main/java/biz/wittkemper/eHotel/utils/pages/BasePage.java
@@ -1,11 +1,9 @@
 package biz.wittkemper.eHotel.utils.pages;
 
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 import biz.wittkemper.eHotel.utils.components.Footer;
-import biz.wittkemper.eHotel.utils.components.Header;
 
 public class BasePage extends WebPage {
 
@@ -16,8 +14,8 @@ public class BasePage extends WebPage {
 	}
 
 	public BasePage(PageParameters parameters) {
-		add(new Header("header"));
-		add(new Label("PageTitle", "eHotel"));
+		// add(new Header("header"));
+		// add(new Label("PageTitle", "eHotel"));
 		add(new Footer("footer"));
 	}
 }
