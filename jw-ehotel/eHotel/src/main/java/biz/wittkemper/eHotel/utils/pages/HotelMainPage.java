1b4b489327a066aa74c6d5a106e9d4f38e9efcc3
joerg@wittkemper.biz <joerg@wittkemper.biz@7fa40d2c-feb0-e616-d173-2ff4c8531f6c>
2014-01-07 20:17:45 +0000

    Bootstrap enreut eingebunden.

--- jw-ehotel/eHotel/src/main/java/biz/wittkemper/eHotel/utils/pages/HotelMainPage.java
+++ jw-ehotel/eHotel/src/main/java/biz/wittkemper/eHotel/utils/pages/HotelMainPage.java
@@ -1,12 +1,9 @@
 package biz.wittkemper.eHotel.utils.pages;
 
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
-import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.html.WebPage;
 
-import de.agilecoders.wicket.core.Bootstrap;
-import de.agilecoders.wicket.core.markup.html.bootstrap.html.HtmlTag;
-import de.agilecoders.wicket.core.markup.html.bootstrap.html.OptimizedMobileViewportMetaTag;
+import biz.wittkemper.eHotel.utils.components.Footer;
 
 @AuthorizeInstantiation("ADMIN")
 public class HotelMainPage extends WebPage {
@@ -15,21 +12,6 @@ public class HotelMainPage extends WebPage {
 
 	public HotelMainPage() {
 
-		// add(new UserPanel("user"));
+		add(new Footer("footer"));
 	}
-
-	@Override
-	protected void onInitialize() {
-		super.onInitialize();
-		add(new HtmlTag("html-tag", getLocale()));
-		add(new OptimizedMobileViewportMetaTag("viewport"));
-	}
-
-	@Override
-	public void renderHead(final IHeaderResponse response) {
-		super.renderHead(response);
-		// add all the JS and CSS for bootstrap
-		Bootstrap.renderHead(response);
-	}
-
 }
